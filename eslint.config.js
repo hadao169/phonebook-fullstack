0c39d59385d38b77901884a3227cc8dd3a996eb2
Dao Thai Ha <daothaiha1692005@gmail.com>
2025-01-12 02:33:34 +0200

    PART 3: Programming a server with NodeJS and Express

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,37 @@
-import globals from 'globals'
-import js from '@eslint/js'
-import stylisticJs from '@stylistic/eslint-plugin-js'
+import globals from "globals";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
-    ignores: ['dist/**', 'node_modules', '.env', 'build/**'],
+    ignores: ["dist/**", "node_modules", ".env", "build/**"],
   },
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
     languageOptions: {
-      sourceType: 'module',
+      sourceType: "module",
       globals: {
         ...globals.node,
       },
-      ecmaVersion: 'latest',
+      ecmaVersion: "latest",
     },
     plugins: {
-      '@stylistic/js': stylisticJs,
+      "@stylistic/js": stylisticJs,
     },
     rules: {
-      '@stylistic/js/indent': ['error', 2],
-      '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
-      eqeqeq: 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
-      'no-console': 'off',
-      'no-unused-vars': 'off',
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/linebreak-style": ["error", "unix"],
+      "@stylistic/js/quotes": ["error", "double"],
+      "@stylistic/js/semi": ["error", "never"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": "off",
+      "no-unused-vars": "off",
+      semi: ["error", "false"],
     },
   },
-]
+];
